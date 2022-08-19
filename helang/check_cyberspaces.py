237ed4a2bda5def508a2c6d44f94c157b0fe042f
Tinhone <tinhone23333@gmail.com>
2022-08-20 05:50:01 +0800

    Revert "Temporary mitigation measures"
    This reverts commit 7462ff290b7d8182ccd1ec109deafb0dfd437301.

--- helang/check_cyberspaces.py
+++ helang/check_cyberspaces.py
@@ -20,11 +20,8 @@ def _get_region() -> str:
     if req.getcode() != 200:
         raise CyberNetworkException(f'request failed with status code {req.getcode()}')
     content = req.read().decode('utf-8')
-    try:
-        ip_list = re.findall(r'[0-9]+(?:\.[0-9]+){3}', content)
-        ip = ip_list[0]
-    except:
-        return -1
+    ip_list = re.findall(r'[0-9]+(?:\.[0-9]+){3}', content)
+    ip = ip_list[0]
 
     try:
         req2 = urllib.request.urlopen(f'https://opendata.baidu.com/api.php?query={ip}&co=&resource_id=6006&oe=utf8', )
@@ -40,9 +37,6 @@ def _get_region() -> str:
 def check_cyberspaces():
     print('Getting your location...')
     region = _get_region()
-    if region == -1:
-        print('Sorry, IPv6 is not supported temporarily')
-        return
     print(f'Your location is {region}.')
     if region in _AMERICAN_REGIONS:
         print('Congratulations! You are in the Cyber Spaces!')
